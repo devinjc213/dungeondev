54ad112a582c406d9433ba537eea177b6a3bbcfb
devinjc213 <devinjc213@gmail.com>
2023-01-04 02:53:03 -0600

    tile editor can export, set up game classes

--- src/main.js
+++ src/main.js
@@ -1,20 +1,145 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 640;
 const SPRITE_WIDTH = 16;
 const SPRITE_HEIGHT = 16;
 const staggerFrames = 5;
 let frameX = 0;
 let gameFrame = 0;
+let enemies = [];
 
 const playerImage = new Image();
-playerImage.src = 'assets/heroes/knight/knight_idle_spritesheet.png';
+playerIdleRight = 'assets/heroes/knight/knight_idle_right.png';
+playerIdleLeft = 'assets/heroes/knight/knight_idle_left.png';
+playerRunRight = 'assets/heroes/knight/knight_run_right.png';
+playerRunLeft = 'assets/heroes/knight/knight_run_left.png';
+playerImage.src = playerIdleRight;
+
+const goblinImage = new Image();
+goblinIdleAnimation = 'assets/enemies/goblin/goblin_idle_spritesheet.png';
+goblinRunningAnimation = 'assets/enemies/goblin/goblin_run_spritesheet.png';
+goblinImage.src = goblinIdleAnimation;
+
+const slimeImage = new Image();
+slimeIdleAnimation = 'assets/enemies/slime/slime_idle_spritesheet.png';
+slimeRunningAnimation = 'assets/enemies/slime/slime_run_spritesheet.png';
+slimeImage.src = slimeIdleAnimation;
+
+const background = new Image();
+background.src = 'assets/maps/map1.png';
+
+class InputHandler {
+  constructor() {
+    this.keys = [];
+    window.addEventListener('keydown',  (e) => {
+      if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+    })
+    window.addEventListener('keyup', e => {
+      this.keys.splice(this.keys.indexOf(e.key), 1);
+    })
+  }
+}
+
+class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.width = 32;
+    this.height = 32;
+    this.x = 0;
+    this.y = 0;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.isFacingRight = true;
+  }
+
+  draw (context) {
+    context.beginPath();
+    context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+    context.stroke();
+    context.drawImage(playerImage, frameX * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT, this.x, this.y, 32, 32);
+  }
+
+  update(input, enemies) {
+    //collision detection
+    enemies.forEach(enemy => {
+      const dx = enemy.x - this.x;
+      const dy = enemy.y - this.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < enemy.width/2 + this.width/2) {
+        console.log('collision detected');
+      }
+    })
+
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    if (input.keys.indexOf('ArrowRight') > -1) this.xSpeed = 3;
+    else if (input.keys.indexOf('ArrowLeft') > -1) this.xSpeed = -3;
+    else this.xSpeed = 0;
+
+    if (input.keys.indexOf('ArrowDown') > -1) this.ySpeed = 3;
+    else if (input.keys.indexOf('ArrowUp') > -1) this.ySpeed = -3;
+    else this.ySpeed = 0;
+
+    //switch between animations
+    if (this.xSpeed > 0) {
+      playerImage.src = playerRunRight;
+      this.isFacingRight = true;
+    }
+    else if (this.xSpeed < 0) {
+      playerImage.src = playerRunLeft;
+      this.isFacingRight = false;
+    }
+    else if (this.xSpeed === 0) playerImage.src = this.isFacingRight ? playerIdleRight : playerIdleLeft;
+
+    //ensure still animating run on y coord movements
+    if (this.ySpeed !== 0 && this.isFacingRight) playerImage.src = playerRunRight;
+    else if (this.ySpeed !== 0 && !this.isFacingRight) playerImage.src = playerRunLeft;
+
+
+    //contain player in boundary
+    if (this.x < 0) this.x = 0;
+    else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+
+    if (this.y < 0) this.y = 0;
+    else if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height
+  }
+}
+
+class Enemy {
+  constructor(gameWidth, gameHeight, image, x, y) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.image = image;
+    this.width = 32;
+    this.height = 32;
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+    context.stroke();
+    context.drawImage(this.image, frameX * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT, this.x, this.y, 32, 32);
+  }
+}
+
+function handleEnemies() {
+  enemies.forEach(enemy => {
+    enemy.draw(ctx);
+  })
+}
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(playerImage, frameX * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, 32, 32);
+  ctx.drawImage(background, 0, 0);
+  handleEnemies();
+  player.draw(ctx);
+  player.update(input, enemies);
 
   if (gameFrame % staggerFrames === 0) {
     if (frameX < 5) frameX++;
@@ -24,5 +149,10 @@ function animate() {
   gameFrame++;
   requestAnimationFrame(animate);
 }
+
+const input = new InputHandler();
+const player = new Player(canvas.width, canvas.height);
+enemies.push(new Enemy(canvas.width, canvas.height, goblinImage, 420, 420));
+enemies.push(new Enemy(canvas.width, canvas.height, slimeImage, 69, 420));
 
 animate();
---
+++ tools/tile_map_editor/script.js
@@ -0,0 +1,165 @@
+window.onload = () => {
+  const canvas = document.getElementsByClassName('editor')[0];
+  const ctx = canvas.getContext('2d');
+  const tilesetContainer = document.querySelector('.tileset-container');
+  const tilesetSelection = document.querySelector('.tileset-img-selection');
+  const tilesetImage = document.querySelector('.tileset-img');
+  const layer0Btn = document.querySelector('#layer-0');
+  const layer1Btn = document.querySelector('#layer-1');
+  const layer2Btn = document.querySelector('#layer-2');
+  const clearCanvasBtn = document.querySelector('#clear-canvas');
+  const exportPNGBtn = document.querySelector('#export-png');
+  const exportJSONBtn = document.querySelector('#export-json');
+
+  //export issue fix
+  tilesetImage.crossOrigin = 'anonymous';
+
+  let isMouseDown = false;
+  let selection = [0, 0];
+  let currentLayer = 0;
+  let layers = [{}];
+
+  let cameraOffset = { x: window.innerWidth/2, y: window.innerHeight/2 }
+  let cameraZoom = 1
+  let MAX_ZOOM = 5
+  let MIN_ZOOM = 0.1
+  let SCROLL_SENSITIVITY = 0.0005
+
+
+
+  //Utility functions
+  function getCoords(e) {
+    const { x, y } = e.target.getBoundingClientRect();
+    const mouseX = e.clientX - x;
+    const mouseY = e.clientY - y;
+    return [Math.floor(mouseX / 32), Math.floor(mouseY / 32)];
+  }
+
+  function clearCanvas() {
+    layers = [{}];
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function setLayer(newLayer) {
+    //Update the layer
+    currentLayer = newLayer;
+  }
+
+  function adjustZoom(zoomAmount) {
+    if (!isMouseDown)
+    {
+      cameraZoom += zoomAmount
+
+      cameraZoom = Math.min( cameraZoom, MAX_ZOOM )
+      cameraZoom = Math.max( cameraZoom, MIN_ZOOM )
+    }
+  }
+
+  function exportPng() {
+    const image = new Image();
+    image.src = canvas.toDataURL('image/png');
+
+    const w = window.open('');
+    w.document.write(image.outerHTML);
+  }
+
+  function exportJson(filename, text) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
+
+
+  //Event listeners
+  canvas.addEventListener('mousedown', () => {
+    isMouseDown = true;
+  });
+  canvas.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+  canvas.addEventListener("mouseleave", () => {
+    isMouseDown = false;
+  });
+  canvas.addEventListener("mousedown", addTile);
+  canvas.addEventListener("mousemove", (event) => {
+    if (isMouseDown) {
+      addTile(event);
+    }
+  });
+  canvas.addEventListener( 'wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY));
+
+  layer0Btn.addEventListener('click', () => setLayer(0));
+  layer1Btn.addEventListener('click', () => setLayer(1));
+  layer2Btn.addEventListener('click', () => setLayer(2));
+  clearCanvasBtn.addEventListener('click', () => clearCanvas());
+  exportPNGBtn.addEventListener('click', () => exportPng());
+  exportJSONBtn.addEventListener('click', () => exportJson('map.json', JSON.stringify(layers)));
+
+  //Select tile from the Tiles grid
+  tilesetContainer.addEventListener("mousedown", (event) => {
+    selection = getCoords(event);
+    tilesetSelection.style.left = selection[0] * 32 + "px";
+    tilesetSelection.style.top = selection[1] * 32 + "px";
+  });
+
+
+
+  //Handler for placing new tiles on the map
+  function addTile(mouseEvent) {
+    let clicked = getCoords(event);
+    let key = clicked[0] + "-" + clicked[1];
+
+    if (mouseEvent.shiftKey) {
+      delete layers[currentLayer][key];
+    } else {
+      if (!layers[currentLayer]) layers.push({ [key]: [selection[0], selection[1]] })
+      else layers[currentLayer][key] = [selection[0], selection[1]];
+    }
+    draw();
+  }
+
+  function draw() {
+    // Translate to the canvas centre before zooming - so you'll always zoom on what you're looking directly at
+    // ctx.translate( canvas.width / 2, canvas.height / 2 )
+    // ctx.scale(cameraZoom, cameraZoom)
+    // ctx.translate( -window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y )
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let size_of_crop = 32;
+
+    layers.forEach((layer) => {
+      Object.keys(layer).forEach((key) => {
+        //Determine x/y position of this placement from key ("3-4" -> x=3, y=4)
+        const positionX = Number(key.split('-')[0]);
+        const positionY = Number(key.split('-')[1]);
+        const [tilesheetX, tilesheetY] = layer[key];
+
+        ctx.drawImage(
+          tilesetImage,
+          tilesheetX * 32,
+          tilesheetY * 32,
+          size_of_crop,
+          size_of_crop,
+          positionX * 32,
+          positionY * 32,
+          size_of_crop,
+          size_of_crop
+        );
+      });
+    });
+  }
+
+  tilesetImage.onload = function () {
+    draw();
+    setLayer(0);
+  }
+};
+
